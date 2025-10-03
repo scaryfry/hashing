9f0144265f15079ad42105e042bd97fce988d10a
scaryfry <scaryfry@gmail.com>
2025-10-03 10:37:27 +0200

    bit of going ahead

--- app.js
+++ app.js
@@ -1,54 +1,62 @@
 import express from "express";
-import cors from "cors";
 import * as db from "./data/db.js";
-import bcrpyt from "bcrpyt";
+import cors from "cors";
+import bcrypt from "bcrypt";
 
 const PORT = 3000;
 const app = express();
-
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-app.use(express.static("public"))
-app.use(bcrpyt());
+app.use(express.static("public"));
 
 app.get("/users", (req, res) => {
-    const users = db.getUsers();
-    res.status(200).json(users);
-})
+  const users = db.getUsers();
+  res.json(users);
+});
 
 app.get("/users/:id", (req, res) => {
-    const user = db.getUserById(+req.params.id)
-    if(!user){
-        return res.status(404).json({message: "User is not found"})
-    }
-    res.status(200).json(user)
-}
-)
-app.post("/users", async (req,res) => {
-    const {email, password} = req.body;
-    if(!email || !password){
-        res.status(404).json({message: "You need to fill all the fields!"})
-    }
-    const salt = await bcrpyt.genSalt();
-    const hashedPassword = await bcrpyt.hash(password, salt)
-    const create = db.saveUser(hashedPassword, email)
-    res.status(200).json(create);
-})
-app.post("/users", async (req,res) => {
-    const {email, password} = req.body;
-    if(!email || !password){
-        res.status(404).json({message: "You need to fill all the fields!"})
-    }
-    const salt = await bcrpyt.genSalt();
-    const hashedPassword = await bcrpyt.hash(password, salt)
-    const create = db.saveUser(hashedPassword, email)
-    res.status(200).json(create);
-})
+  const user = db.getUserById(+req.params.id);
+  if (!user) {
+    res.status(404).json({ message: "User not found" });
+  }
+  res.json(user);
+});
 
-app.use((err, res, req, next) => {
-    if(err) res.status(500).json({error: err.message})
-})
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Invalid data" });
+    }
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const saved = db.saveUser(email, hashedPassword);
+    const user = db.getUserById(saved.lastInsertRowid);
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-app.listen(PORT, ()=> {
-    console.log(`Server runs on port ${PORT}`)
-})
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  const user = db.getUserByEmail(email);
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  if (!bcrypt.compareSync(password, user.password)) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  res.json(user);
+});
+
+app.use((err, req, res, next) => {
+  if (err) res.status(500).json({ error: err.message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server runs on port ${PORT}`);
+});
---
+++ public/script.js
@@ -0,0 +1,61 @@
+const data = [];
+function loginConfirm() {
+  const email = document.getElementById("email-login").value;
+  const password = document.getElementById("password-login").value;
+
+  console.log(email, password);
+  if (
+    sessionStorage.getItem("email") == email &&
+    sessionStorage.getItem("password") == password
+  ) {
+    alert("Sikeres bejelentkezés");
+    document.getElementById("email-login").value = "";
+    document.getElementById("password-login").value = "";
+    return;
+  } else {
+    alert("A jelszó vagy az email nem helyes");
+    return;
+  }
+}
+function showRegisterPage() {
+  document.getElementById("password-register").type = "password";
+  document.getElementById("password-repeat").type = "password";
+  document.getElementById("password-login").type = "hidden";
+  document.getElementById("login-button").style.display = "none";
+  document.getElementById("register-page").style.display = "none";
+  document.getElementById("register-button").style.display = "block";
+  document.getElementById("login-page").style.display = "block";
+}
+function showLoginPage() {
+  document.getElementById("password-register").type = "hidden";
+  document.getElementById("password-repeat").type = "hidden";
+  document.getElementById("password-login").type = "password";
+  document.getElementById("register-button").style.display = "none";
+  document.getElementById("login-page").style.display = "none";
+  document.getElementById("login-button").style.display = "block";
+  document.getElementById("register-page").style.display = "block";
+}
+document.getElementById("register-button").addEventListener("click", () => {
+  const email = document.getElementById("email-login").value;
+  const password = document.getElementById("password-register").value;
+  const repeat = document.getElementById("password-repeat").value;
+  if (password != repeat) {
+    alert("Nem egyezik meg a két jelszó!");
+    return;
+  }
+  data.push(email, password);
+  console.log(data);
+  if (
+    sessionStorage.getItem("email") == "" ||
+    sessionStorage.getItem("password") == ""
+  ) {
+    alert("Minden mező kitöltése kötelező!");
+  }
+  alert("Sikeres a regisztráció");
+  sessionStorage.setItem("email", email);
+  sessionStorage.setItem("password", password);
+  sessionStorage.setItem("repeat", repeat);
+  document.getElementById("password-register").value = "";
+  document.getElementById("password-repeat").value = "";
+  showLoginPage();
+});
